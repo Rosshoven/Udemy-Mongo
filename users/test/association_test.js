a673c27bfef67d6cdf4df4be8965fe4c96cac8a7
Rosshoven <Michaeledwardross@gmail.com>
2023-10-26 14:02:52 -0500

    wrote association_test

--- users/test/association_test.js
+++ users/test/association_test.js
@@ -1,5 +1,70 @@
 const mongoose = require('mongoose');
+const assert = require('assert');
+
 const User = require('../src/user_schema');
 const Comment = require('../src/comment');
 const BlogPost = require('../src/blogPost');
 
+
+describe('Associations', () => {
+    
+    // rememeber scope for your variables, put outside so it's more gloabl
+    // lower case because it is an instance of that models. Model are above in Upper case, lines 2,3,4
+    let joe, blogPost, comment;
+
+
+    beforeEach((done) => {
+        joe = new User({ name:'Joe' });
+        blogPost = new BlogPost({title: 'JS is greatS', content: 'No, Like, For Real For Real'});
+        comment = new Comment({content: 'OH BOY Congrats on the POAST'});
+
+        // Have to get these to assiociate with each other. Remember you have blogPost in User Schema
+        // we're pushing in an etntire model
+        // the array from the models makes it possible to push
+        joe.blogPosts.push(blogPost);
+        blogPost.comments.push(comment);
+        comment.user = joe;
+
+        // joe.save();
+        // blogPost.save();
+        // comment.save();
+        // Use Promise.all because you have 3 promises above with .save(). Promise all takes an array of promises and puts into single Promise
+        Promise.all([joe.save(), blogPost.save(), comment.save()])
+            .then(() => done());
+    }); 
+
+    // if I want to run only a sngle test by itslef (we have 21 already), use  it.only
+    it('saves a relation between a user and a blogpost', (done) => {
+        User.findOne({name: 'Joe'})
+            .populate('blogPosts')
+            .then((user) => {
+                assert(user.blogPosts[0].title === 'JS is greatS')
+                console.log(user);
+                done();
+         });
+    });
+
+    it('saves a full relation tree', (done) => {
+        User.findOne({name: 'Joe'})
+         .populate({ 
+            path: 'blogPosts',
+            populate: {
+                path: 'comments',
+                // when we load up the comments, we want to use the comment model
+                model: 'comment',
+                populate: {
+                        path: 'user',
+                        model: 'user'
+                    }
+            }
+         })
+         .then((user) => {
+            // console.log(user.blogPosts[0].comments[0]);
+            assert(user.name === 'Joe');
+            assert(user.blogPosts[0].title === 'JS is greatS');
+            assert(user.blogPosts[0].comments[0].content === 'OH BOY Congrats on the POAST');
+            assert(user.blogPosts[0].comments[0].user.name === 'Joe');
+            done();
+         })
+    });
+});
